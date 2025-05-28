9e4513cb961d2abbba88cba617b42ecfdb1b80dd
jamesxddev <jamescabral.dev@gmail.com>
2025-05-28 15:47:42 +0800

    Added Axios Update on Login ButtonClick call login endpoint

--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -1,14 +1,25 @@
-<script lang="ts">
-export const description
-  = 'A simple login form with email and password. The submit button says \'Sign in\'.'
-export const iframeHeight = '5000px'
-export const containerClass = 'w-full h-screen flex items-center justify-center px-4'
-</script>
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useAuthStore } from '@/store/auth'
+
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+
+const username = ref('')
+const password = ref('')
+const error = ref('')
+const auth = useAuthStore()
+
+async function onSignIn() {
+  try {
+    error.value = ''
+    await auth.login(username.value, password.value)
+  } catch {
+    error.value = 'Invalid credentials'
+  }
+}
 </script>
 <template>
   <Card class="w-full max-w-sm">
@@ -23,15 +34,15 @@ import { Label } from '@/components/ui/label'
     <CardContent class="grid gap-4">
       <div class="grid gap-2">
         <Label for="email">Username</Label>
-        <Input id="email" type="email" placeholder="username" required />
+        <Input id="email" type="email" placeholder="username" required v-model="username"/>
       </div>
       <div class="grid gap-2">
         <Label for="password">Password</Label>
-        <Input id="password" type="password" required />
+        <Input id="password" type="password" required v-model="password"/>
       </div>
     </CardContent>
     <CardFooter>
-      <Button class="w-full">
+      <Button class="w-full" @click="onSignIn">
         Sign in
       </Button>
     </CardFooter>
