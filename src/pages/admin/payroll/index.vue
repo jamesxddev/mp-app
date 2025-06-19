68a74a97c8fecef5ebce61dadc61cd0dbe4eb7c8
jamesxddev <jamescabral.dev@gmail.com>
2025-06-19 17:02:00 +0800

    added simple table

--- src/pages/admin/payroll/index.vue
+++ src/pages/admin/payroll/index.vue
@@ -10,9 +10,64 @@ import Header from '@/components/Header.vue';
 
 import { Combobox, ComboboxAnchor, ComboboxEmpty, ComboboxGroup, ComboboxInput, ComboboxItem, ComboboxItemIndicator, ComboboxList, ComboboxTrigger } from '@/components/ui/combobox'
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
 const frameworks = [
   { value: 'Cha', label: 'Cha' },
   { value: 'Ally', label: 'Ally' }
+]
+
+const invoices = [
+  {
+    invoice: 'INV001',
+    paymentStatus: 'Paid',
+    totalAmount: '$250.00',
+    paymentMethod: 'Credit Card',
+  },
+  {
+    invoice: 'INV002',
+    paymentStatus: 'Pending',
+    totalAmount: '$150.00',
+    paymentMethod: 'PayPal',
+  },
+  {
+    invoice: 'INV003',
+    paymentStatus: 'Unpaid',
+    totalAmount: '$350.00',
+    paymentMethod: 'Bank Transfer',
+  },
+  {
+    invoice: 'INV004',
+    paymentStatus: 'Paid',
+    totalAmount: '$450.00',
+    paymentMethod: 'Credit Card',
+  },
+  {
+    invoice: 'INV005',
+    paymentStatus: 'Paid',
+    totalAmount: '$550.00',
+    paymentMethod: 'PayPal',
+  },
+  {
+    invoice: 'INV006',
+    paymentStatus: 'Pending',
+    totalAmount: '$200.00',
+    paymentMethod: 'Bank Transfer',
+  },
+  {
+    invoice: 'INV007',
+    paymentStatus: 'Unpaid',
+    totalAmount: '$300.00',
+    paymentMethod: 'Credit Card',
+  },
 ]
 
 const value = ref<typeof frameworks[0]>()
@@ -22,7 +77,7 @@ const value = ref<typeof frameworks[0]>()
     <SidebarInset>
         <Header />
 
-        <div class="flex flex-1 flex-col gap-4 p-4">
+        <div class="flex flex-col gap-4 p-4">
             <div>
                 <Combobox v-model="value" by="label">
                     <ComboboxAnchor as-child>
@@ -60,6 +115,36 @@ const value = ref<typeof frameworks[0]>()
                     </ComboboxList>
                 </Combobox>
             </div>
+        </div>
+
+        <div class="flex flex-col gap-4 p-4">
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead class="w-[100px]">
+                            Invoice
+                        </TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>Method</TableHead>
+                        <TableHead class="text-right">
+                            Amount
+                        </TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow v-for="invoice in invoices" :key="invoice.invoice">
+                        <TableCell class="font-medium">
+                            {{ invoice.invoice }}
+                        </TableCell>
+                        <TableCell>{{ invoice.paymentStatus }}</TableCell>
+                        <TableCell>{{ invoice.paymentMethod }}</TableCell>
+                        <TableCell class="text-right">
+                            {{ invoice.totalAmount }}
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
         </div>
     </SidebarInset>
 </template>
